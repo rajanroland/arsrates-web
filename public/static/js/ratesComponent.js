c148378fead7c2265917341032bb5c2bc5a0fd28
Roland <roland@rolandwebdev.com>
2025-04-20 17:31:02 +0000

    Update from production build 2025-04-20T17:31:02.339Z

--- public/static/js/ratesComponent.js
+++ public/static/js/ratesComponent.js
@@ -51,7 +51,7 @@ const RateChangeDisplay = ({ change }) => {
           fontSize: "0.875rem",
           whiteSpace: "nowrap",
           display: "inline-block",
-          minWidth: "54px",
+          minWidth: "70px",
           textAlign: "left",
         },
         className: isPositive ? "text-success" : "text-danger",
